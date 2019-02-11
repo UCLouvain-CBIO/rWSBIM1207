9340dd4dd9ed744422f8e7c63858c3402702e5d6
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-02-11 22:59:25 +0100

    add tcga data

--- R/datafiles.R
+++ R/datafiles.R
@@ -3,3 +3,16 @@
 beers.csv <- function()
     dir(system.file("extdata", package = "rWSBIM1207"),
         full.names = TRUE, pattern = "beers.csv")
+
+
+##' @rdname tcga
+##' @export
+clinical1.csv <- function()
+    dir(system.file("extdata", package = "rWSBIM1207"),
+        full.names = TRUE, pattern = "clinical1.csv")
+
+##' @rdname tcga
+##' @export
+expression.csv <- function()
+    dir(system.file("extdata", package = "rWSBIM1207"),
+        full.names = TRUE, pattern = "expression.csv")
