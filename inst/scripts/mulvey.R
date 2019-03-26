eea393e41d5a32e3a7fdb5e6671321daa7170385
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-03-26 21:11:56 +0100

    add more vars to mvylng

--- inst/scripts/mulvey.R
+++ inst/scripts/mulvey.R
@@ -1,8 +1,15 @@
 library("pRolocdata")
+library("tidies")
 data(mulvey2015)
 
 mvylng <- as_tibble(mulvey2015) %>%
     select(Accession, markers, sample, exprs, Description)
+
+
+mvylng$time <- as.numeric(sub("XEN", 96,
+                              sub("hr", "",
+                                  sub("rep.+_", "", mvylng$sample))))
+mvylng$rep <- as.numeric(sub("rep", "", sub("_.+$", "", mvylng$sample)))
 
 save(mvylng, file = "../../data/mvylng.rda",
      compress = "xz", compression_level = 9)
